dd34ccfbcae98657aec87aa0055213b277e95073
Carlton Hanna <channa@figure.com>
2021-11-09 10:10:54 -0700

    change proposal command slightly

--- simple_msg_based_fee_proposal.sh
+++ simple_msg_based_fee_proposal.sh
@@ -4,7 +4,7 @@ MSG_TYPE=/cosmos.bank.v1beta1.MsgSend
 PROPOSAL_ID=1
 
 echo "ADDING PROPOSAL RIGHT NOW"
-${PROVENANCE_DEV_DIR}/build/provenanced tx msgfees proposal add "adding" "adding msgsend fee"  10000000000nhash --msg-type "${MSG_TYPE}" --amount 1nhash --additional-fee 1999999999nhash  --from validator --yes -t --home ${PROVENANCE_DEV_DIR}/build/run/provenanced --fees 1000000000nhash --chain-id testing --keyring-backend test -o json | jq
+${PROVENANCE_DEV_DIR}/build/provenanced tx msgfees proposal add "adding" "adding msgsend fee"  10000000000nhash --msg-type "${MSG_TYPE}" --additional-fee 1999999999nhash  --from validator --yes -t --home ${PROVENANCE_DEV_DIR}/build/run/provenanced --fees 1000000000nhash --chain-id testing --keyring-backend test -o json | jq
 echo "VOTING ON PROPOSAL"
 ${PROVENANCE_DEV_DIR}/build/provenanced tx gov vote ${PROPOSAL_ID} yes --from validator --yes -t --home ${PROVENANCE_DEV_DIR}/build/run/provenanced --fees 1000000000nhash --chain-id testing --keyring-backend test -o json | jq
 echo "AFTER VOTING"
