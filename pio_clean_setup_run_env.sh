d2c7a26d4f3707522e6ca659c0a2655bf42d10b7
Matthew Witkowski <mwitkowski@figure.com>
2022-09-12 23:19:49 -0400

    Added additional variables to make configuration easier.

--- pio_clean_setup_run_env.sh
+++ pio_clean_setup_run_env.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-PROVENANCE_DEV_DIR=~/code/provenance
+
+# Provenance Configuration
+PROVENANCE_DEV_DIR=~/work/provenance
+PROVENANCE_HOME=${PROVENANCE_DEV_DIR}/build/run/provenanced
+CONFIG=run-config
 VOTING_PERIOD=20s
 MINUTE_EPOCH="{\"identifier\": \"minute\",\"start_height\": \"0\",\"duration\": \"12\",\"current_epoch\": \"0\", \"current_epoch_start_height\": \"0\",\"epoch_counting_started\": false}"
 
@@ -9,7 +13,7 @@ cd ${PROVENANCE_DEV_DIR}
 go mod vendor
 make clean 
 make build 
-make run-config 
-cat ${PROVENANCE_DEV_DIR}/build/run/provenanced/config/genesis.json | jq ' .app_state.gov.voting_params.voting_period="'${VOTING_PERIOD}'" ' | tee ${PROVENANCE_DEV_DIR}/build/run/provenanced/config/genesis.json 
+make ${CONFIG}
+cat ${PROVENANCE_HOME}/config/genesis.json | jq ' .app_state.gov.voting_params.voting_period="'${VOTING_PERIOD}'" ' | tee ${PROVENANCE_HOME}/config/genesis.json 
 #cat ${PROVENANCE_DEV_DIR}/build/run/provenanced/config/genesis.json | jq --arg minute_epoch "$MINUTE_EPOCH" ' .app_state.epoch.epochs += ['$'] ' | tee ${PROVENANCE_DEV_DIR}/build/run/provenanced/config/genesis.json
-cat ${PROVENANCE_DEV_DIR}/build/run/provenanced/config/genesis.json | grep voting
+cat ${PROVENANCE_HOME}/config/genesis.json | grep voting
