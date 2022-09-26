0e57e36ed4318dd207a72813d28780641065eb13
Matthew Witkowski <mwitkowski@figure.com>
2022-09-26 16:06:16 -0400

    Removed temporary scripts to ease provenance use. Updated configuration for scripts.

--- simple/get_keys.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Provenance Configuration
-SCRIPTS_DIR=~/code/pio-scratch
-PROVENANCE_SCRIPT=$SCRIPTS_DIR/simple/provenance.sh
-source $PROVENANCE_SCRIPT
-
-keys list
--- simple_msg_based_fee_proposal.sh
+++ simple_msg_based_fee_proposal.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
+
+#Provenance Configuration
 PROVENANCE_DEV_DIR=~/code/provenance
+CHAIN_ID=testing
+FROM=validator
+PROPOSAL_FEE=1000000000vspn
+DEPOSIT=10000000000vspn
+
+# Fee Configuration
 MSG_TYPE=/cosmos.bank.v1beta1.MsgSend
-PROPOSAL_ID=1
+MSG_FEE=5ibc/319937B2FDA7A07031DBE22EA76C34CAC9DCFBD9AA1A922FA2B87421107B545D
+
+PROVENANCE_BIN="$PROVENANCE_DEV_DIR/build/provenanced"
+PROVENANCE_HOME="$PROVENANCE_DEV_DIR/build/run/provenanced"
+PROPOSAL_ID=$($PROVENANCE_BIN -t --home $PROVENANCE_HOME q gov proposals | grep "id:" | wc -l | sed 's/ //g')
+PROPOSAL_ID=$(echo $(( $PROPOSAL_ID + 1 )))
 
 echo "ADDING PROPOSAL RIGHT NOW"
-${PROVENANCE_DEV_DIR}/build/provenanced tx msgfees proposal add "adding" "adding msgsend fee"  10000000000nhash --msg-type "${MSG_TYPE}" --additional-fee 2000000000nhash  --from validator --yes -t --home ${PROVENANCE_DEV_DIR}/build/run/provenanced --fees 1000000000nhash --chain-id testing --keyring-backend test -o json | jq
+"$PROVENANCE_BIN" tx msgfees proposal add "adding" "adding msgsend fee" "$DEPOSIT" --msg-type "${MSG_TYPE}" --additional-fee $MSG_FEE  --from $FROM --yes -t --home ${PROVENANCE_DEV_DIR}/build/run/provenanced --fees "$PROPOSAL_FEE" --chain-id $CHAIN_ID --keyring-backend test -o json | jq
 echo "VOTING ON PROPOSAL"
-${PROVENANCE_DEV_DIR}/build/provenanced tx gov vote ${PROPOSAL_ID} yes --from validator --yes -t --home ${PROVENANCE_DEV_DIR}/build/run/provenanced --fees 1000000000nhash --chain-id testing --keyring-backend test -o json | jq
+"$PROVENANCE_BIN" tx gov vote "$PROPOSAL_ID" yes --from $FROM --yes -t --home $PROVENANCE_HOME --fees "$PROPOSAL_FEE" --chain-id $CHAIN_ID --keyring-backend test -o json | jq
 echo "AFTER VOTING"
